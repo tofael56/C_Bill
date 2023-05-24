9c28bc88f20240132953309c3e487f06daa24720
Tofael Ahmed <tofaelahmed1756@gmail.com>
2023-05-24 15:28:43 +0600

    ANGULARJS ADDED

---
+++ Pages/CustomerBill/CustomerBillFormCtrl.js
@@ -0,0 +1,8 @@
+﻿app.controller("CustomerBillFormCtrl", function ($scope, $cookieStore, $http, $filter) {
+    Clear();
+    function Clear() {
+        $scope.custoemrBill = {};
+        $scope.custoemrBillDetailList = [];
+
+    }
+}
---
+++ Pages/app.js
@@ -0,0 +1,1 @@
+﻿var app = angular.module('CustomerBillApp', ['ngAnimate', 'ui.bootstrap', 'components']);
